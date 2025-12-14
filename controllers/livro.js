90b216329e40c82a795d047f6538c21793816f7a
Eryck Assis <eng.assis.dev@gmail.com>
2025-12-14 13:28:15 -0300

    fix: improve ID validation in getLivro function

--- controllers/livro.js
+++ controllers/livro.js
@@ -19,25 +19,27 @@ function getLivro(req, res) {
   try {
     const id = req.params.id;
 
-    if (id && Number(id)) {
-      const livro = getLivroPorId(id);
-      res.send(livro);
-    } else {
-      res.status(422);
-      res.send("ID inválido");
+    if (!id || Number(id)) {
+      return res.status(422).send("ID Invalido");
     }
+    const livro = getLivroPorId(id);
+    res.send(livro);
   } catch (error) {
-    res.status(500);
-    res.send(error.message);
+    res.status(500).send(error.message);
   }
 }
 
 function postLivro(req, res) {
   try {
     const livroNovo = req.body;
-    insereLivro(livroNovo);
-    res.status(201);
-    res.send("Livro foi inserido");
+    if (req.body.nome) {
+      insereLivro(livroNovo);
+      res.status(201);
+      res.send("Livro foi inserido");
+    } else {
+      res.status(422);
+      res.send("Nome Invalido");
+    }
   } catch (error) {
     res.status(500);
     res.send(error.message);
